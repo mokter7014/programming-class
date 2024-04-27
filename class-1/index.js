1cb2e74f3251dda5fe2acb9de301faec3c7727f1
Md. Mokter <md.mokter@apsissolutions.com>
2024-04-27 12:56:27 +0600

    initial commit

---
+++ class-1/index.js
@@ -0,0 +1,15 @@
+// var friends = "Mokter,Jahid,Nur,Jobayer";
+// var friendArr = friends.split(",");
+
+// var textConcatInArr = friendArr.map((item) => {
+//   return item + " " + "Hossain";
+// });
+// // console.log(friends.split(","));
+
+// console.log(textConcatInArr);
+
+var friendArray = ["Mokter", "Joveyer", "Jahid", "Nur"];
+var friendString = friendArray.join();
+
+console.log(typeof friendArray);
+console.log(typeof friendString);
