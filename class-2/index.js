7059a534f5ed015b20f8fe87be8a8d9aacad2dfb
mokter7082 <md.mokter@apsissolutions.com>
2024-04-27 22:35:01 +0600

    second class

---
+++ class-2/index.js
@@ -0,0 +1,14 @@
+var num1 = 1.51;
+// var num2 = 15;
+var result = num1 + num2;
+
+// console.log(result);
+console.log(`Result ${result}`);
+
+// console.log("num1", typeof parseFloat(num1));
+
+// console.log(Math.floor(num1));
+// console.log(Math.ceil(num1));
+
+// console.log(Math.round());
+console.log(Math.random() * 100);
